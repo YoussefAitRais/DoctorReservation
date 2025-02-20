0bdbf462af1917860eb888a2cb8524e8da966a71
Youssef Ait Rais <copybuisness007@gmail.com>
2025-02-20 10:51:43 +0200

    patientservlet

--- src/main/java/com/code/Web/PatientServlet.java
+++ src/main/java/com/code/Web/PatientServlet.java
@@ -1,4 +1,43 @@
 package com.code.Web;
 
-public class PatientServlet {
+import com.code.DAO.PatientDAO;
+import com.code.Model.Patient;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/patient")
+public class PatientServlet extends HttpServlet {
+    private PatientDAO patientDAO = new PatientDAO();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Patient> patients = patientDAO.getAllPatients();
+        request.setAttribute("patients", patients);
+        request.getRequestDispatcher("WEB-INF/views/patients.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String phone = request.getParameter("phone");
+        String adresse = request.getParameter("adresse");
+
+        Patient patient = new Patient(name, phone, adresse);
+        patientDAO.addPatient(patient);
+
+        response.sendRedirect("patient");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        patientDAO.deletePatient(id);
+        response.sendRedirect("patient");
+    }
 }
